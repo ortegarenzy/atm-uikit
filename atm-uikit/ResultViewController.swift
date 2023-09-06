4fa691fa5bd3f0a133c4d91a291ba4383eb20a7f
Renzy Ortega <devortegarenzy@gmail.com>
2023-09-06 23:28:19 +0800

    feat: add new assets, add logic for handling results

--- atm-uikit/ResultViewController.swift
+++ atm-uikit/ResultViewController.swift
@@ -10,26 +10,37 @@ import Foundation
 
 class ResultViewController: UIViewController {
     
+    @IBOutlet weak var thousandLabel: UILabel!
+    @IBOutlet weak var fiveHundredLabel: UILabel!
+    @IBOutlet weak var oneHundredLabel: UILabel!
     var amount: Int?
     var delegate: ResultViewControllerDelegate?
+    let thousand: Int = 1000
+    let fiveHundred: Int = 500
+    let oneHundred: Int = 100
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         guard let amount else { return }
-        print("amount withdrawn \(amount)")
         let result = breakdownAmount(amount)
-
-        for (denomination, count) in result {
-            print("\(denomination) x \(count)")
-        }
+        let thousandResult = processResult(result: result, type: thousand)
+        thousandLabel.text = String(thousandResult)
+        let fiveHundredResult = processResult(result: result, type: fiveHundred)
+        fiveHundredLabel.text = String(fiveHundredResult)
+        let oneHundredResult = processResult(result: result, type: oneHundred)
+        oneHundredLabel.text = String(oneHundredResult)
+    }
+    
+    func processResult(result: [Int: Int], type: Int) -> Int {
+        guard let amount = result[type] else { return 0 }
+        return amount
     }
     
     func breakdownAmount(_ amount: Int) -> [Int: Int] {
         var remainingAmount = amount
         var breakdown: [Int: Int] = [:]
         
-        let denominations = [1000, 500, 100]
+        let denominations = [thousand, fiveHundred, oneHundred]
         
         for denomination in denominations {
             let count = remainingAmount / denomination
